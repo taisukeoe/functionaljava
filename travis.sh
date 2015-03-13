89b114ce78f3bc80dd7bd1c8631618c494618d6e
taisukeoe <oeuia.t@gmail.com>
2015-03-13 07:33:28 -0700

    run travis

--- travis.sh
+++ travis.sh
@@ -2,3 +2,4 @@ export JAVA8_HOME=$ORACLEJDK8_JAVA_HOME &&
 export JAVA7_HOME=$ORACLEJDK7_JAVA_HOME &&
 export JAVA_HOME=$ORACLEJDK7_JAVA_HOME &&
 ./gradlew build -Dorg.gradle.java.home=$ORACLEJDK8_JAVA_HOME
+
