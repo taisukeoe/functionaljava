0ab5ffd14f8d8834f2dd706f92e439ecf1278e0c
taisukeoe <oeuia.t@gmail.com>
2015-03-13 07:16:10 -0700

    fix Unsupported major.minor version bug

--- travis.sh
+++ travis.sh
@@ -1,4 +1,4 @@
 export JAVA8_HOME=$ORACLEJDK8_JAVA_HOME &&
 export JAVA7_HOME=$ORACLEJDK7_JAVA_HOME &&
 export JAVA_HOME=$ORACLEJDK7_JAVA_HOME &&
-./gradlew build
+./gradlew build -Dorg.gradle.java.home=$ORACLEJDK8_JAVA_HOME
